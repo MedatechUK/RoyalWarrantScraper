bf53b2226e2f9351cec587baa61ef36c41537ebd
Greg Molnar <greg.molnar@mwrisecurity.com>
2014-08-01 13:08:28 +0100

    slight refactoring + fix encoding issue

--- lib/royal_warrant_scraper.rb
+++ lib/royal_warrant_scraper.rb
@@ -1,37 +1,37 @@
-#Royal Warrant web scraper 
+#Royal Warrant web scraper
 
 require 'nokogiri'
 require 'net/http'
 require_relative 'royal_warrant_entry.rb'
 
-class RoyalWarrantScraper 
+class RoyalWarrantScraper
   #class for scraping RoyalWarrantEntries from Royal Warrant site
   attr_reader :url, :params, :response, :trades
 
   def initialize
     #frame wrapped by the official search page
-    @url = "http://rwha.bwdata.co.uk/RWHAPublic/RWHA_Public_Search.php" 
+    @url = "http://rwha.bwdata.co.uk/RWHAPublic/RWHA_Public_Search.php"
     @trades = []
-    get_trades 
+    get_trades
   end
 
   def scrape(q="", grantor="-All-", trade="-All-", region="-All-")
 
-  
-    @response = Nokogiri::HTML(Net::HTTP.post_form(URI.parse(@url),
-                                                  {"q"       => q, 
+    response = Net::HTTP.post_form(URI.parse(@url),
+                                                  {"q"       => q,
                                                    "grantor" => grantor,
                                                    "trade"   => trade,
                                                    "region"  => region})
-                                                  .body)
+                                                  .body.encode("UTF-8", invalid: :replace, undef: :replace, replace: "")
+    html = Nokogiri::HTML(response)
 
     #look for the div with results text, grab the proceeding table element
-    search_results = @response.xpath("//div[text() = 'Results of your search:']")[0].next_element
-    
-    royal_warrant_holders = [] 
-    
+    search_results = html.xpath("//div[text() = 'Results of your search:']")[0].next_element
+
+    royal_warrant_holders = []
+
     search_results.xpath("tr").each do |tr|
-      next if tr.css("h2").size == 0 
+      next if tr.css("h2").size == 0
       name = tr.css("h2").text
       description = tr.css("div")[0].xpath("text()").to_s.strip!.gsub!("\n", "<break>")
       phone = tr.xpath(".//td[text() = 'Phone:']")[0].next_element.text
@@ -40,11 +40,11 @@ class RoyalWarrantScraper
       website = tr.xpath(".//td[text() = 'Website:']")[0].next_element.text
 
 
-      royal_warrant_holders << RoyalWarrantEntry.new(name, 
-                                                     description, 
-                                                     phone, 
-                                                     fax, 
-                                                     email, 
+      royal_warrant_holders << RoyalWarrantEntry.new(name,
+                                                     description,
+                                                     phone,
+                                                     fax,
+                                                     email,
                                                      website)
 
     end
@@ -53,24 +53,24 @@ class RoyalWarrantScraper
 
   def scrape_to_csv(file_path, q="", grantor="-All-", trade="-All-", region="-All-")
     write_to_csv(file_path, scrape(q, grantor, trade, region))
-  end 
- 
+  end
 
-  private 
+
+  private
   def get_trades
-    @response = Nokogiri::HTML(Net::HTTP.get_response(URI.parse(@url)).body)
-    @response.css("select[name='trade']/option").each { |option| @trades << option["value"] }    
-  end 
+    response = Nokogiri::HTML(Net::HTTP.get_response(URI.parse(@url)).body)
+    response.css("select[name='trade']/option").each { |option| @trades << option["value"] }
+  end
 
   def write_to_csv(file_path, entries)
-    CSV.open(file_path, "w", 
-             write_headers: true, 
+    CSV.open(file_path, "w",
+             write_headers: true,
              headers: ["name", "description", "phone", "fax", "email", "website"]
              ) do |csv|
       entries.each { |entry| csv << entry.to_a }
     end
-  end   
-end 
+  end
+end
 
 
 
